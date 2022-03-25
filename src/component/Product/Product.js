06d7937602703f2a0a38d43b549acc29d7fdd2fb
coderrakibul <coder.rakibul@gmail.com>
2022-03-26 01:23:47 +0600

    icon added

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
-    const {name, img, price, id} = props.product;
+    const {name, img, price} = props.product;
     
 
     return (
@@ -9,7 +11,11 @@ const Product = (props) => {
             <img className='w-100' src={img} alt="" />
             <h4>{name}</h4>
             <h5>Price: ${price}</h5>
-            <button className='btn btn-primary'>Add To Cart</button>
+            <button className='btn btn-primary d-flex justify-center align-items-center'>
+                <h6 className='me-2 mb-0'>Add To Cart</h6>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+                
+            </button>
         </div>
     );
 };
