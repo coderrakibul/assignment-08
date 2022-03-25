1f95f66c099fdfc0157b40a1328edf8065af8a14
coderrakibul <coder.rakibul@gmail.com>
2022-03-25 23:08:40 +0600

    header added

---
+++ src/component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className=' mt-4'>
+            <h1 className='text-center'>BD <strong className='text-warning'>Cars </strong> Corner</h1>
+        </div>
+    );
+};
+
+export default Header;
