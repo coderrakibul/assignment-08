0a5af0705457200fe385dcd7f070b933a0e223ff
coderrakibul <coder.rakibul@gmail.com>
2022-03-26 04:51:10 +0600

    some styles added

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Product.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
@@ -9,9 +10,9 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img className='w-100' src={img} alt="" />
-            <h4>{name}</h4>
+            <h4 className='mt-3'>{name}</h4>
             <h5>Price: ${price}</h5>
-            <button className='btn btn-primary d-flex justify-center align-items-center'>
+            <button className='add-button btn btn-primary w-100 d-flex align-items-center'>
                 <h6 className='me-2 mb-0'>Add To Cart</h6>
                 <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
                 
