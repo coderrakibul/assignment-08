67592243113aa613edc28b3eefa41568e9b850a7
coderrakibul <coder.rakibul@gmail.com>
2022-03-26 15:39:04 +0600

    remove button activated

--- src/component/Products/Products.js
+++ src/component/Products/Products.js
@@ -19,6 +19,10 @@ const Products = () => {
         setCart(newCart);
     }
 
+    const removeItem = () => {
+        document.getElementById("remove-item").innerHTML = [];
+    }
+
     return (
         <div className='main-container'>
             <div className='products-container'>
@@ -33,17 +37,18 @@ const Products = () => {
 
             <div className='cart-container'>
                 <h4 className='text-center mt-3 mb-3'>Selected Cars</h4>
-
+                <div id='remove-item'>
                 {
-                    cart.map(product => <Cart
+                         cart.map(product => <Cart
                         key={product.id}
                         name={product.name}
                         img={product.img}
 
                     ></Cart>)
                 }
+                </div>
                 <button className='btn mt-3 btn-success fw-bold'>Choose 1 For Me</button>
-                <button className='btn mt-3 btn-warning fw-bold'>Choose Again</button>
+                <button onClick={removeItem} className='btn mt-3 btn-warning fw-bold'>Reset</button>
             </div>
 
         </div>
